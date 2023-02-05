e022e35bc959dcac0dccd50dc969ace0c0c1419b
ZOOEEER <hanyl19@mails.tsinghua.edu.cn>
2023-02-05 10:25:38 +0800

    0205 edit the util_prot.py, add (*args, **kwargs) parameters

--- util/util_prot.py
+++ util/util_prot.py
@@ -32,7 +32,7 @@ def get_swiss_model_configs():
         }
     }
 
-def submit_project(sequence:str = ""):
+def submit_project(sequence:str = "", *args, **kwargs):
     configs = get_swiss_model_configs()
     data = {
         "target_sequences": sequence,
@@ -40,61 +40,76 @@ def submit_project(sequence:str = ""):
     }
     url = configs["host"] + configs["urls"]["automodel"]
     project_info = None
-    try:
-        body = requests.post(url, json.dumps(data), headers = configs["headers"])
-        if body.status_code in [200, 202]:
-            # Success, deal with the body
-            project_info = json.loads(body.text)
-            logging.info(f"Submitted job:{project_info}")
-    except requests.exceptions.ConnectionError:
-        logging.info(f"Connection error.")
-    except:
-        logging.info(f"Unknown error.")
+    body = _requests_post(url, data, *args, **kwargs)
+    if body and body.status_code in [200, 202]:
+        # Success, deal with the body
+        project_info = json.loads(body.text)
     return project_info
 
-def _requests_get(url:str) -> requests.models.Response:
-    configs = get_swiss_model_configs()
-    body = None
-    try:
-        body = requests.get(url, headers = configs["headers"])
-        logging.info(f"Requests.get:(url={url}")
-    except requests.exceptions.ConnectionError:
-        logging.info(f"Connection error.")
-    except:
-        logging.info(f"Unknown error.")
-    return body
-
-def get_project_result(project_id:str) -> dict:
+def get_project_result(project_id:str, *args, **kwargs) -> dict:
     configs = get_swiss_model_configs()
     url = configs["host"] + configs["urls"]["full-details"].format(project_id)
     project_info = None
-    body = _requests_get(url)
+    body = _requests_get(url, *args, **kwargs)
     if body.status_code in [200]:
         # Success, parse the body
         project_info = json.loads(body.text)
     return project_info
 
-def dump_project_info(path:str, project_info:dict) -> None:
+def dump_project_info(path:str, project_info:dict, *args, **kwargs) -> None:
     with open(path, 'w') as f:
         json.dump(project_info, f, ensure_ascii=True, indent=4, )
     status = project_info["status"] if "status" in project_info.keys() else ""
-    logging.info(f'Save the project({project_info["project_id"]}, {status}) info to {path}.')
+    if kwargs["verbose"]:
+        logging.info(f'Save the project({project_info["project_id"]}, {status}) info to {path}.')
     return path
 
-def download_model(path:str, project_id:str, model_id:int=1) -> None:
+def download_model(path:str, project_id:str, model_id:int=1, *args, **kwargs) -> None:
     configs = get_swiss_model_configs()
     url = configs["host"] + f'/project/{project_id}/models/0{model_id}.pdb'
-    body = _requests_get(url)
+    body = _requests_get(url, *args, **kwargs)
     if body.status_code in [200]:
         # Success, save the body
         with open(path, 'w') as f:
             f.write(body.text)
-        logging.info(f"Save the model {model_id} to {path}.")
+        if kwargs["verbose"]:
+            logging.info(f"Save the model {model_id} to {path}.")
     return path
+
+def _requests_post(url:str, data:dict, *args, **kwargs) -> requests.models.Response:
+    configs = get_swiss_model_configs()
+    body = None
+    try:
+        body = requests.post(url, json.dumps(data), headers = configs["headers"])
+        if kwargs["verbose"]:
+           logging.info(f"Requests.post:{url}, {data}")
+    except requests.exceptions.ConnectionError:
+        if kwargs["verbose"]:
+            logging.info(f"Connection error.")
+    except:
+        if kwargs["verbose"]:
+            logging.info(f"Unknown error.")
+    return body
+
+def _requests_get(url:str, *args, **kwargs) -> requests.models.Response:
+    configs = get_swiss_model_configs()
+    body = None
+    try:
+        body = requests.get(url, headers = configs["headers"])
+        if kwargs["verbose"]:
+            logging.info(f"Requests.get:(url={url}")
+    except requests.exceptions.ConnectionError:
+        if kwargs["verbose"]:
+            logging.info(f"Connection error.")
+    except:
+        if kwargs["verbose"]:
+            logging.info(f"Unknown error.")
+    return body
 
 def query_enzymes(
     enzymes:pd.DataFrame,
     pdbdir:Optional[str] = None,
+    *args, **kwargs
 ):
     """
     Homology Model by Swiss-Model.
@@ -114,31 +129,11 @@ def query_enzymes(
         sequence = enzymes.loc[i, "sequence"]
         project_id = enzymes.loc[i, "project_id"]
         if project_id == "":
-            project_info = submit_project(sequence = sequence)
+            project_info = submit_project(sequence = sequence, *args, **kwargs)
             enzymes.loc[i, "project_id"] = project_info["project_id"]
         else:
-            project_info = get_project_result(project_id = project_id)
+            project_info = get_project_result(project_id = project_id, *args, **kwargs)
             if len(project_info["models"]):
                 model_id = 0
-                download_model(path = os.path.join(pdbdir, f"{i}.pdb"), project_id = project_id, model_id = model_id)
-        dump_project_info(path = os.path.join(pdbdir, f"{i}.json"), project_info = project_info)
-
-
-
-    # try to fetch project
-    
-
-    #
-
-
-# def get_token(
-
-# ):
-#     configs = get_swiss_model_configs()
-#     url = configs["host"] + configs["urls"]["api-token-auth"]
-#     data = {
-#         "username": configs["username"],
-#         "password": configs["password"]
-#     }
-#     body = requests.post(url, data = data)
-#     return body
+                download_model(path = os.path.join(pdbdir, f"{i}.pdb"), project_id = project_id, model_id = model_id, *args, **kwargs)
+        dump_project_info(path = os.path.join(pdbdir, f"{i}.json"), project_info = project_info, *args, **kwargs)
